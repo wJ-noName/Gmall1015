ccc2b33d48fd49fae9a50148c595e4b1dd07719c
wuyaowang <wuyaowang@163.com>
2019-02-28 13:54:07 +0800

    搭建框架

---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/impl/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.atguigu.gmall.user.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserAddressMapper;
+import com.atguigu.gmall.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserInfoMapper userInfoMapper;
+
+    @Autowired
+    UserAddressMapper userAddressMapper;
+
+    @Override
+    public List<UserInfo> getAllUser() {
+        List<UserInfo> userInfos = userInfoMapper.selectAllUser();
+
+        return userInfos;
+    }
+
+    @Override
+    public List<UserAddress> getAllAddress() {
+
+        List<UserAddress> userAddresses = userAddressMapper.selectAll();
+
+        return userAddresses;
+    }
+
+    @Override
+    public List<UserInfo> allUser() {
+
+        return userInfoMapper.selectAll();
+    }
+}
